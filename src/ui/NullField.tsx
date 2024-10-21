bec7ef588a7d82c8cf0e2bbf06bd8f51d3d86d6e
Manu <manutepowa@gmail.com>
2024-10-21 12:17:46 +0200

    src to development

--- src/ui/NullField.tsx
+++ src/ui/NullField.tsx
@@ -1,5 +1,4 @@
-"use client"
-import type { UIFieldClient } from "payload";
+import type { UIField } from "payload";
 import React from "react"
 
-export const NullField: React.FC<UIFieldClient> = () => null
+export const NullField: React.FC<UIField> = () => null
